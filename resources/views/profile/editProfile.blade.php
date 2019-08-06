2e0df270b21ebed0f28ed81dee4edecd02b86847
Umesh Kumar <umesh.kumar21@outlook.com>
2019-08-06 14:59:52 +0530

    errror fixed for non-object

--- resources/views/profile/editProfile.blade.php
+++ resources/views/profile/editProfile.blade.php
@@ -28,43 +28,43 @@
                   </div>
                   <div class="col-md-6">
                     <label for="website">Website</label>
-                    <input type="text" name="website" class="form-control" value="{{ isset($usersProfile->website) && $usersProfile->website != '' ? $usersProfile->website : '' }}" placeholder="Any website you own?">
+                    <input type="text" name="website" class="form-control" value="{{ isset($usersProfile) && isset($usersProfile->website) && $usersProfile->website != '' ? $usersProfile->website : '' }}" placeholder="Any website you own?">
                   </div>
                 </div>
                 <br>
                 <div class="row">
                   <div class="col-md-6">
                     <label for="twitter">Twitter Username</label>
-                    <input type="text" name="twitter" class="form-control" value="{{ isset($usersProfile->twitter) && $usersProfile->twitter != '' ? $usersProfile->twitter : '' }}" placeholder="Let's add you twitter account...">
+                    <input type="text" name="twitter" class="form-control" value="{{ isset($usersProfile) && isset($usersProfile->twitter) && $usersProfile->twitter != '' ? $usersProfile->twitter : '' }}" placeholder="Let's add you twitter account...">
                   </div>
                   <div class="col-md-6">
                     <label for="github">GitHub Username</label>
-                    <input type="text" name="github" class="form-control" value="{{ isset($usersProfile->github) && $usersProfile->github != '' ? $usersProfile->github : '' }}" placeholder="How about adding github profile too?">
+                    <input type="text" name="github" class="form-control" value="{{ isset($usersProfile) && isset($usersProfile->github) && $usersProfile->github != '' ? $usersProfile->github : '' }}" placeholder="How about adding github profile too?">
                   </div>
                 </div>
                 <br>
                 <div class="row">
                   <div class="col-md-6">
                     <label for="company">Company</label>
-                    <input type="text" name="company" class="form-control" value="{{ isset($usersProfile->company) && $usersProfile->company != '' ? $usersProfile->company : '' }}" placeholder="Where do you work?">
+                    <input type="text" name="company" class="form-control" value="{{ isset($usersProfile) && isset($usersProfile->company) && $usersProfile->company != '' ? $usersProfile->company : '' }}" placeholder="Where do you work?">
                   </div>
                   <div class="col-md-6">
                     <label for="job_title">Job Title</label>
-                    <input type="text" name="job_title" class="form-control" value="{{ isset($usersProfile->job_title) && $usersProfile->job_title != '' ? $usersProfile->job_title : '' }}" placeholder="What's your designation? ex: Developer, Designer, etc.">
+                    <input type="text" name="job_title" class="form-control" value="{{ isset($usersProfile) && isset($usersProfile->job_title) && $usersProfile->job_title != '' ? $usersProfile->job_title : '' }}" placeholder="What's your designation? ex: Developer, Designer, etc.">
                   </div>
                 </div>
                 <br>
                 <div class="row">
                   <div class="col-md-6">
                     <label for="hometown">Hometown</label>
-                    <input type="text" name="hometown" class="form-control" value="{{ isset($usersProfile->hometown) && $usersProfile->hometown != '' ? $usersProfile->hometown : '' }}" placeholder="Where do you live?">
+                    <input type="text" name="hometown" class="form-control" value="{{ isset($usersProfile) && isset($usersProfile->hometown) && $usersProfile->hometown != '' ? $usersProfile->hometown : '' }}" placeholder="Where do you live?">
                   </div>
                   <div class="col-md-6">
                     <label for="country_id">Country</label>
                     <select class="form-control" name="country_id" required>
                       <option value="">Select Country</option>
                       @foreach($countries as $country)
-                        <option value="{{ $country->id }}" {{ ($country->id == $usersProfile->country_id) ? 'selected' : '' }}>{{ $country->name }}</option>
+                        <option value="{{ $country->id }}" {{ (isset($usersProfile) && $country->id == $usersProfile->country_id) ? 'selected' : '' }}>{{ $country->name }}</option>
                       @endforeach
                     </select>
                   </div>
